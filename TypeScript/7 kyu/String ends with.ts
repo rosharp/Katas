cd85cc962ae49fe29272230afea2621e368d640b
rosharp <ro.moreko@gmail.com>
2022-06-28 09:44:31 +1000

    TS: String ends with

---
+++ TypeScript/7 kyu/String ends with.ts
@@ -0,0 +1,5 @@
+export function solution(str: string, ending: string): boolean {
+	return str.substring(str.length - ending.length) === ending ? true : false;
+}
+
+console.log(solution('abcde', 'cee'));
